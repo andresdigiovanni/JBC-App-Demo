cd64cae12d7acfd04578fa0eb38aa1e57d8737af
Andrés Di Giovanni <andresdigiovanni@gmail.com>
2020-05-19 15:57:27 +0200

    Initial commit

---
+++ precache-manifest.c7073a7408791d72fbdaec8ff2e088dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7169cb8e2f2dab26a9144bf323b9b381",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e76a907df481fd21431",
+    "url": "./static/css/2.d6500151.chunk.css"
+  },
+  {
+    "revision": "ead89823cc307f029a72",
+    "url": "./static/css/main.2d77c966.chunk.css"
+  },
+  {
+    "revision": "9e76a907df481fd21431",
+    "url": "./static/js/2.cd3ee045.chunk.js"
+  },
+  {
+    "revision": "2bbdcd6adfa622d5bd559ac285d47096",
+    "url": "./static/js/2.cd3ee045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ead89823cc307f029a72",
+    "url": "./static/js/main.76d97d47.chunk.js"
+  },
+  {
+    "revision": "725bdc4ac7a4b0926f5c",
+    "url": "./static/js/runtime-main.bd0908bb.js"
+  }
+]);
