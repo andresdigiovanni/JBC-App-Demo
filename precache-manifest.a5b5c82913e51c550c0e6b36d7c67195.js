a3b27f9212fe16b60068b9e942e530de9b5587a0
Andrés Di Giovanni <andresdigiovanni@gmail.com>
2020-05-20 07:56:04 +0200

    Changes

---
+++ precache-manifest.a5b5c82913e51c550c0e6b36d7c67195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36944bca9bd2e1ce2e57e0d697ae4e69",
+    "url": "/JBC-App-Demo/index.html"
+  },
+  {
+    "revision": "9e76a907df481fd21431",
+    "url": "/JBC-App-Demo/static/css/2.d6500151.chunk.css"
+  },
+  {
+    "revision": "c486870f25db0868a662",
+    "url": "/JBC-App-Demo/static/css/main.2d77c966.chunk.css"
+  },
+  {
+    "revision": "9e76a907df481fd21431",
+    "url": "/JBC-App-Demo/static/js/2.cd3ee045.chunk.js"
+  },
+  {
+    "revision": "2bbdcd6adfa622d5bd559ac285d47096",
+    "url": "/JBC-App-Demo/static/js/2.cd3ee045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c486870f25db0868a662",
+    "url": "/JBC-App-Demo/static/js/main.85d695d4.chunk.js"
+  },
+  {
+    "revision": "8a7d27d5715c671428e7",
+    "url": "/JBC-App-Demo/static/js/runtime-main.ed011cc7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c7073a7408791d72fbdaec8ff2e088dd.js"
+  "/JBC-App-Demo/precache-manifest.a5b5c82913e51c550c0e6b36d7c67195.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JBC-App-Demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
